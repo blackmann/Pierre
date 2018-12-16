15b087c67ed1ddb13ce5b8184bcee34250af5ad0
DeGreat Yartey <yarteydegreat2@gmail.com>
2018-12-16 06:51:10 +0000

    UI adjustments

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,33 @@
-var typespace = document.getElementById("typespace")
+$(document).ready(() => {
+    var header = $('#main-nav')
+    header.hide()
 
-var typewriter = new Typewriter(typespace, {loop: true})
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > 450) {
+            header.fadeIn()
+        } else {
+            header.fadeOut()
+        }
+    })
+})
 
-typewriter.typeString("A mobile app developer")
-    .pauseFor(500)
-    .deleteAll()
-    .typeString("An Oracle APEX expert")
-    .pauseFor(500)
-    .deleteAll()
-    .typeString("A web designer")
-    .pauseFor("500")
-    .deleteAll()
-    .typeString("A nice gentleman ☺️")
-    .start()
+
+function startTyping() {
+    var typespace = document.getElementById("typespace")
+
+    var typewriter = new Typewriter(typespace, {loop: true})
+    
+    typewriter.typeString("A mobile app developer")
+        .pauseFor(500)
+        .deleteAll()
+        .typeString("An Oracle APEX expert")
+        .pauseFor(500)
+        .deleteAll()
+        .typeString("A leader in the Official Oracle Forum")
+        .pauseFor("500")
+        .deleteAll()
+        .typeString("A beautiful web designer")
+        .start()
+}
+
+startTyping()
